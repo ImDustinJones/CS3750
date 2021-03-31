1cafd0725fe9815e00344425276922ca6448a37c
ImDustinJones <70123697+ImDustinJones@users.noreply.github.com>
2021-03-31 12:00:01 -0600

    Finished the CoursesDAOTest.java

--- ProjectDemo/tests/java/com/example/ProjectDemo/CoursesDAOTest.java
+++ ProjectDemo/tests/java/com/example/ProjectDemo/CoursesDAOTest.java
@@ -26,6 +26,8 @@ class CoursesDAOTest {
             courseCount = resultSet.getInt("count");
         }
         int firstCount = courseCount;
+        connection.close();
+        Connection connection2 = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
 
         //Insert New Course Into System
         String courseNumber = "6789";
@@ -44,28 +46,33 @@ class CoursesDAOTest {
         String thursday = "0";
         String friday = "0";
         String sqlInsert = "INSERT INTO courseList(courseNumber, courseName, departmentCode, instructorEmail, instructorLastName, courseDescription, creditHours, startTime, endTime, studentCapacity, monday, tuesday, wednesday, thursday, friday) VALUES('"+courseNumber+"','"+courseName+"','"+departmentCode+"','"+instructorEmail+"','"+instructorLastName+"','"+courseDescription+"','"+creditHours+"','"+startTime+"','"+endTime+"','"+studentCapacity+"','"+monday+"','"+tuesday+"','"+wednesday+"','"+thursday+"','"+friday+"');";
-        PreparedStatement statementInsert = connection.prepareStatement(sqlInsert);
+        PreparedStatement statementInsert = connection2.prepareStatement(sqlInsert);
         int result = statementInsert.executeUpdate();
+        connection2.close();
+        Connection connection3 = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
 
         //Count the current courses in the system again
         String queryCount = "SELECT COUNT(courseID) as count FROM courseList";
-        PreparedStatement statement2 = connection.prepareStatement(queryCount);
+        PreparedStatement statement2 = connection3.prepareStatement(queryCount);
         ResultSet resultSetCount = statement2.executeQuery();
         int courseCount2 = 0;
         if(resultSetCount.next()){
-            courseCount2 = resultSet.getInt("count");
+            courseCount2 = resultSetCount.getInt("count");
         }
         int secondCount = courseCount2;
-        secondCount++;
 
         //Compare the counts to verify one is only one value larger
         if(firstCount != secondCount-1){
             fail("Database Values were not correct. The first count was "+firstCount+" and the second count was "+secondCount);
         }
+
+        connection3.close();
+        Connection connection4 = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
         //Remove the newest course
         String queryDelete = "DELETE FROM courseList WHERE courseNumber = '6789';";
-        PreparedStatement statementDelete = connection.prepareStatement(queryDelete);
+        PreparedStatement statementDelete = connection4.prepareStatement(queryDelete);
         statementDelete.executeUpdate();
+        connection4.close();
 
     }
 }
